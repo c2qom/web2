658869263aec3ae2e003e8c261a2058759a78540
c2qom <c2kkqom@gmail.com>
2023-02-27 18:55:13 +0700

    upload tugas1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import Header from "./components/Header";
- 
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
- 
-  const clickMe = (name) => {
-    console.log('Hello: '+ name);
-  }
- 
   return (
-    <div>
-      <Header />
-      <button onClick={ () => clickMe('Fikri') }>Click Me</button>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
- 
+
 export default App;
